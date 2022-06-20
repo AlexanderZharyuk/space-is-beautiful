e19741b9b03eefaa432a4ec80c17ce58ceee76c1
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-06-20 16:30:37 +0300

    Download picture to path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import telegram
 import requests
@@ -6,5 +7,17 @@ import requests
 from dotenv import load_dotenv
 
 
+def download_image(image_url: str, image_folder: str = 'images/') -> None:
+    response = requests.get(image_url)
+
+    os.makedirs(image_folder, exist_ok=True)
+    image_name = 'hubble.jpeg'
+    image_path = os.path.join(image_folder, image_name)
+
+    with open(image_path, 'wb') as new_image:
+        new_image.write(response.content)
+
+
 if __name__ == '__main__':
     load_dotenv()
+    download_image('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg', 'test/')
