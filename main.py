f737c60fba0312a41e4e9c1fef237d1b257e2e62
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-06-20 17:54:00 +0300

    Download EPIC photos from NASA

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 import os
 import pprint
 
+from datetime import datetime
 from urllib.parse import urlparse
 
 import telegram
@@ -58,7 +59,28 @@ def download_apod():
             download_image(image_url_from_api, image_name=image_name)
 
 
+def download_epic():
+    all_images_url = 'https://api.nasa.gov/EPIC/api/natural/images'
+    params = {
+        'api_key': os.environ['NASA_API_KEY'],
+    }
+    response = requests.get(all_images_url, params=params).json()
+
+    for image_number, image in enumerate(response):
+        date = image['date']
+        image_name = image['image']
+        photo_datetime = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+
+        url = f'https://api.nasa.gov/EPIC/archive/natural/{photo_datetime.year}/' \
+              f'{photo_datetime.month:02}/{photo_datetime.day}/png/{image_name}.png'
+        response = requests.get(url, params=params)
+
+        image_extension = get_file_extension(response.url)
+        saved_image_name = f'EPIC_{image_number}{image_extension}'
+        download_image(response.url, saved_image_name)
+
+
 if __name__ == '__main__':
     load_dotenv()
-    download_apod()
-
+    # download_apod()
+    # download_epic()
