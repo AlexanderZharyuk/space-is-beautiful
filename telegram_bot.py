91c7e325e50548843c710dbd83ff30e134047d19
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-06-21 16:50:07 +0300

    Add exception when image size is large

--- telegram_bot.py
+++ telegram_bot.py
@@ -19,20 +19,20 @@ if __name__ == '__main__':
 
     bot = telegram.Bot(telegram_token)
     images = os.walk('images').__next__()[2]
-    max_file_size = 20000000
-
     logging.basicConfig(format='[%(levelname)s]: %(message)s', datefmt='%m.%d.%Y %H:%M:%S', level=logging.INFO)
 
     while True:
         try:
             random_image = random.choice(images)
-            image_path = f'images/{random_image}'
-            file_size = os.path.getsize(image_path)
-            if file_size >= max_file_size:
-                continue
+            image_path = os.path.join(os.environ['DOWNLOAD_DIR'], random_image)
 
             bot.send_photo(chat_id=telegram_group_id, photo=open(image_path, 'rb'))
-            time.sleep(posting_delay)
         except ConnectionError:
             logging.error('ConnectionError. Going to sleep 1 min.')
             time.sleep(60)
+            continue
+        except telegram.error.BadRequest:
+            logging.info('Image size is big, try new one...')
+            continue
+
+        time.sleep(posting_delay)
