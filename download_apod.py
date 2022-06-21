c6a79d696fdf93e80e806544ca0ef509aa5a1d64
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-06-21 15:59:49 +0300

    Add logging and fix getting image url from api

--- download_apod.py
+++ download_apod.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import logging
 
 import requests
 
@@ -16,11 +17,12 @@ def download_apod():
         'count': 30
     }
 
-    response = requests.get(url, params=params).json()
+    response = requests.get(url, params=params)
+    response.raise_for_status()
 
-    for image_number, image_url in enumerate(response):
+    for image_number, image_url in enumerate(response.json()):
         if image_url.get('hdurl'):
-            image_url_from_api = image_url['hdurl']
+            image_url_from_api = image_url['hdurl'].replace('%20', '')
             image_extension = get_file_extension(image_url_from_api)
             image_name = f'nasa_apod_{image_number}{image_extension}'
             download_image(image_url_from_api, image_name=image_name)
@@ -30,4 +32,11 @@ if __name__ == '__main__':
     load_dotenv()
     parser = argparse.ArgumentParser(description='Script for download APOD photos.')
     parser.parse_args()
-    download_apod()
+
+    logging.basicConfig(format='[%(levelname)s]: %(message)s', datefmt='%m.%d.%Y %H:%M:%S', level=logging.INFO)
+    try:
+        download_apod()
+    except ConnectionError:
+        logging.error('ConnectionError. Try again.')
+    except requests.HTTPError:
+        logging.warning('Get HTTPError, some troubles with host?')
